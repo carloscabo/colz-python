6027b7d11651d89fcd8edb65d76f9be87a4ffa7b
Carlos Cabo <carloscabo@gmail.com>
2016-06-02 22:19:16 +0200

    Bug en hslToRgb...

--- colz.py
+++ colz.py
@@ -5,24 +5,24 @@ class Colz:
         """ Class initialization """
         self.reset()
 
-    def reset(self):
+    def reset ( self ):
         """ Cleans all color components """
-        self.hex = None
-        self.r = None
-        self.g = None
-        self.b = None
-        self.h = None
-        self.s = None
-        self.l = None
+        self.hex = ''
+        self.r = 0.0
+        self.g = 0.0
+        self.b = 0.0
+        self.h = 0.0
+        self.s = 0.0
+        self.l = 0.0
         self.a = 1.0
-        self.rgb = None
-        self.hsl = None
-        self.rgba = None
-        self.hsla = None
+        self.rgb = []
+        self.hsl = []
+        self.rgba = []
+        self.hsla = []
         return self
 
     def setHex ( self, hex ):
-        """ Creates color from hex value """
+        """ Creates color object from hex value """
         self.reset()
 
         hex = hex.lower().lstrip('#')
@@ -33,7 +33,7 @@ class Colz:
             hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
         rgb = []
         for i in range(0, 6, 2):
-            rgb.append( int(hex[i:i+2], 16)/255 )
+            rgb.append( int(hex[i:i+2], 16) / 255.0 )
 
         self.hex = hex
         self.r = rgb[0]
@@ -42,21 +42,21 @@ class Colz:
         self.rgb = [ *rgb ]
         self.rgba = [ *rgb, self.a ]
 
-        self.calculateHslFromRgb( *rgb )
+        self.updateFromRgb( *rgb )
 
     def setRgb ( self, r, g, b ):
-        """ Create color object from rgb values """
+        """ Creates color object from rgb values """
         self.setRgba( r, g, b )
 
     def setRgba ( self, r, g, b, a = 1.0 ):
-        """ Creates color from rgb values value """
+        """Creates color object from rgba values """
         self.reset()
 
-        # If any component is int ( 0 - 255 ) convert to float ( 0 - 1)
+        # If any component is int ( 0 - 255 ) convert to float ( 0 - 1 )
         rgba = [ r, g, b, a ]
         for i in range(len(rgba)):
             if  isinstance( rgba[i], int ):
-                rgba[i] = rgba[i]/255
+                rgba[i] = rgba[i] / 255.0
 
         self.r = rgba[0]
         self.g = rgba[1]
@@ -64,9 +64,9 @@ class Colz:
         self.a = rgba[3]
         self.rgb = [ rgba[0], rgba[1], rgba[2] ]
         self.rgba = rgba
-        self.hex = "%02x%02x%02x" % (int(rgba[0]*255), int(rgba[1]*255), int(rgba[2]*255))
+        self.hex = Colz.rgbToHex( rgba[0], rgba[1], rgba[2] )
 
-        self.calculateHslFromRgb( rgba[0], rgba[1], rgba[2] )
+        self.updateFromRgb()
 
     def setHsl ( self, h, s, l ):
         """ Create color object from hsl values """
@@ -80,68 +80,244 @@ class Colz:
         hsla = [ h, s, l, a ]
         for i in range(len(hsla)):
             if  isinstance( hsla[i], int ):
-                if i == 0:
-                    hsla[i] = hsla[i]/360
-                else:
-                    hsla[i] = hsla[i]/100
+                hsla[i] = hsla[i] / 360.0 if i == 0 else hsla[i] / 100.0
 
         self.h = hsla[0]
         self.s = hsla[1]
         self.l = hsla[2]
         self.a = hsla[3]
-        self.hsl = [ hsla[0], hsla[1], hsla[2] ]
-        self.rgba = hsla
+        self.hsl  = [ hsla[0], hsla[1], hsla[2] ]
+        self.hsla = [ hsla[0], hsla[1], hsla[2], hsla[3] ]
 
-        self.calculateRgbFromHsl( h, s, l )
+        self.updateFromHsl()
 
-    def hue2rgb (p, q, t):
-        if t < 0:
-             t += 1
-        if t > 1:
-            t -= 1
-        if t < 1 / 6:
-            return p + (q - p) * 6 * t
-        if t < 1 / 2:
-            return q
-        if t < 2 / 3:
-            return p + (q - p) * (2 / 3 - t) * 6
+    def setHsv ( self, h, s, b ):
+        self.setHsb( h, s, b )
+    def setHsb ( self, h, s, b ):
+        """ Create color object from hsb / hsv values """
+        self.reset()
+        rgb = Colz.hsbToRgb( h, s, b )
+        self.setRgba( *rgb )
 
-    def calculateHslFromRgb( self, r, g, b):
-        hsl = self.rgbToHsl( r, g, b )
+    def updateFromRgb ( self ):
+        hsl = self.rgbToHsl( self.r, self.g, self.b )
         self.h = hsl[0]
         self.s = hsl[1]
         self.l = hsl[2]
-        self.hsl = [ hsl[0], hsl[1], hsl[2] ]
-        self.hsla = [ hsl[0], hsl[1], hsl[2], self.a ]
+        self.hsl  = hsl
+        self.hsla = [ *hsl, self.a ]
 
-    def rgbToHsl( self, r, g, b ):
-        _max = max(r, g, b)
-        _min = min(r, g, b)
-        l = (_max + _min) / 2
+    def updateFromHsl ( self ):
+        rgb = Colz.hslToRgb( self.h, self.s, self.l )
+        self.r = rgb[0]
+        self.g = rgb[1]
+        self.b = rgb[2]
+        self.rgb = rgb
+        self.rgba = [ *rgb, self.a ]
+        # Updates Hex
+        # self.hex = Colz.rgbToHex( self.r, self.g, self.b )
+
+    @staticmethod
+    def hue2rgb ( p, q, t ):
+        if t < 0.0:
+             t += 1.0
+        if t > 1.0:
+            t -= 1.0
+        if t < 1.0 / 6.0:
+            return p + ( q - p ) * 6.0 * t
+        if t < 1.0 / 2.0:
+            return q
+        if t < 2.0 / 3.0:
+            return p + ( q - p ) * ( 2.0 / 3.0 - t ) * 6.0
+
+    @staticmethod
+    def rgbToHsl( r, g, b ):
+        _max = max( r, g, b )
+        _min = min( r, g, b )
+        l = (_max + _min) / 2.0
 
         if _max == _min:
-            h = 0 # achromatic
-            s = 0
+            h = s = 0.0 # achromatic
         else:
             d = _max - _min
-            s = d / (2 - _max - _min) if l > 0.5 else d / (_max + _min)
+            s = d / ( 2.0 - _max - _min ) if l > 0.5 else d / (_max + _min)
 
             if _max == r:
-                h = (g - b) / d + ( 6 if g < b else 0 )
+                h = ( g - b ) / d + ( 6.0 if g < b else 0.0 )
             elif _max == g:
-                h = (b - r) / d + 2
+                h = ( b - r ) / d + 2.0
             else: # max == b
-                h = (r - g) / d + 4
-            h /= 6
+                h = ( r - g ) / d + 4.0
+            h /= 6.0
         return [ h, s, l ]
 
-    def hslToRgb( h, s, l ):
+    @staticmethod
+    def hslToRgb ( h, s, l ):
         if s == 0.0:
             r = g = b = l # achromatic
         else:
-            q = l * (1 + s) if l < 0.5 else l + s - l * s
-            p = 2 * l - q
-            r = self.hue2rgb(p, q, h + 1 / 3)
-            g = self.hue2rgb(p, q, h)
-            b = self.hue2rgb(p, q, h - 1 / 3)
+            q = l * ( 1.0 + s ) if l < 0.5 else l + s - l * s
+            p = 2.0 * l - q
+            r = Colz.hue2rgb( p, q, h + 1.0 / 3.0 )
+            g = Colz.hue2rgb( p, q, h )
+            b = Colz.hue2rgb( p, q, h - 1.0 / 3.0 )
+        print(r)
         return [ r, g, b ]
+
+    @staticmethod
+    def rgbToHsv ( r, g, b ):
+        Colz.rgbToHsb( r, g, b )
+
+    @staticmethod
+    def rgbToHsb ( r, g, b ):
+        _max = max( r, g, b )
+        _min = min( r, g, b )
+        v = _max
+        d = _max - _min;
+        s = 0.0 if _max == 0.0 else d / _max
+
+        if _max == _min:
+            h = 0.0 # achromatic
+        else:
+            if _max == r:
+                h = (g - b) / d + ( 6.0 if g < b else 0.0 )
+            elif _max == g:
+                h = (b - r) / d + 2.0
+            elif _max == b:
+                h = (r - g) / d + 4.0
+            h /= 6.0
+        return [ h, s, v ]
+
+    @staticmethod
+    def rgbToHex ( r, g, b ):
+        return "%02x%02x%02x" % ( int( r * 255.0 ), int( g * 255.0 ), int( b * 255.0 ))
+
+    @staticmethod
+    def rgbToHsb ( r, g, b ):
+
+        if  isinstance( r, int ):
+            r /= 255.0
+        if  isinstance( g, int ):
+            g /= 255.0
+        if  isinstance( b, int ):
+            b /= 255.0
+
+        _max = max( r, g, b )
+        _min = min( r, g, b )
+        v = _max
+
+        d = _max - _min
+        s = 0.0 if max == 0.0 else d / _max
+
+        if _max == _min:
+            h = 0.0 # achromatic
+        else:
+            if _max == r:
+                h = ( g - b ) / d + ( 6.0 if g < b else 0.0 )
+            elif _max == g:
+                h = ( b - r ) / d + 2.0
+            elif _max == b:
+                h = (r - g) / d + 4.0
+            h /= 6.0
+
+            # map top 360, 100, 100
+            # h = int( round( h * 360 ) )
+            # s = int( round( s * 100 ) )
+            # v = int( round( v * 100 ) )
+
+        return [ h, s, v ]
+
+    @staticmethod
+    def hsbToRgb ( h, s, v ):
+
+        if  isinstance( h, int ):
+            h /= 360.0
+        if  isinstance( s, int ):
+            s /= 100.0
+        if  isinstance( v, int ):
+            v /= 100.0
+
+        if v == 0.0:
+            return [0, 0, 0]
+
+        h = h * 6.0
+        i = int( math.floor( h ) )
+
+        f = h - i
+        p = v * ( 1.0 - s )
+        q = v * ( 1.0 - ( s * f ) )
+        t = v * ( 1.0 - ( s * ( 1.0 - f ) ) )
+
+        if i == 0:
+            r = v
+            g = t
+            b = p
+        elif i == 1:
+            r = q
+            g = v
+            b = p
+        elif i == 2:
+            r = p
+            g = v
+            b = t
+        elif i == 3:
+            r = p
+            g = q
+            b = v
+        elif i == 4:
+            r = t
+            g = p
+            b = v
+        elif i == 5:
+            r = v
+            g = p
+            b = q
+        # To return int
+        # r = int( math.floor( r * 255 ) )
+        # g = int( math.floor( g * 255 ) )
+        # b = int( math.floor( b * 255 ) )
+
+        return [ r, g, b ]
+
+    # @staticmethod
+    # def hsvToHsl ( h, s, b ):
+    #     """ This is an alias to hsbToHsl """
+    #     Colz.hsbToHsl ( h, s, b )
+
+    @staticmethod
+    def hsbToHsl ( h, s, b ):
+        """ Converts Hsv / Hsb to Rgb """
+        return Colz.rgbToHsl( Colz.hsbToRgb( h, s, b ) )
+
+    # Utils, setters
+
+    def setHue ( self, newhue ):
+        if  isinstance( newhue, int ):
+            newhue /= 360.0
+        self.h = newhue
+        self.hsl[0]  = newhue
+        self.hsla[0] = newhue
+        self.updateFromHsl()
+
+    def setSat ( self, newsat ):
+        if  isinstance( newsat, int ):
+            newsat /= 100.0
+        self.s = newsat
+        self.hsl[1]  = newsat
+        self.hsla[1] = newsat
+        self.updateFromHsl()
+
+    def setLum ( self, newlum ):
+        if  isinstance( newlum, int ):
+            newlum /= 100
+        self.l = newlum
+        self.hsl[2]  = newlum
+        self.hsla[2] = newlum
+        self.updateFromHsl()
+
+    def setAlpha ( self, newalpha ):
+        if  isinstance( newalpha, int ):
+            raise ValueError('Expects a float value in the [ 0.0 - 1.0 ] range!')
+        self.a = newalpha
+        self.hsla[3] = newalpha
+        self.rgba[3] = newalpha
